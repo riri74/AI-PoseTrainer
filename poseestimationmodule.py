f34f429ffd3194afe95221fc606df77fae6576c5
Chirantana Prashanth <112489735+riri74@users.noreply.github.com>
2024-10-02 18:20:18 +0530

    poseestimationmodule.py

--- poseestimationmodule.py
+++ poseestimationmodule.py
@@ -1,4 +1,4 @@
-import cv2
+import cv2 as cv
 import mediapipe as mp
 import time
 import math
@@ -19,7 +19,7 @@ class poseDetector():
                              min_tracking_confidence=self.trackCon)
 
     def findPose(self, img, draw=True):
-        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
         self.results = self.pose.process(imgRGB)
         if self.results.pose_landmarks:
             if draw:
@@ -35,7 +35,7 @@ class poseDetector():
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 self.lmList.append([id, cx, cy])
                 if draw:
-                    cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+                    cv.circle(img, (cx, cy), 5, (255, 0, 0), cv.FILLED)
         return self.lmList
     def findAngle(self, img, p1, p2, p3, draw=True):
         # Get the landmarks
@@ -50,37 +50,37 @@ class poseDetector():
         # print(angle)
         # Draw
         if draw:
-            cv2.line(img, (x1, y1), (x2, y2), (255, 255, 255), 3)
-            cv2.line(img, (x3, y3), (x2, y2), (255, 255, 255), 3)
-            cv2.circle(img, (x1, y1), 10, (0, 0, 255), cv2.FILLED)
-            cv2.circle(img, (x1, y1), 15, (0, 0, 255), 2)
-            cv2.circle(img, (x2, y2), 10, (0, 0, 255), cv2.FILLED)
-            cv2.circle(img, (x2, y2), 15, (0, 0, 255), 2)
-            cv2.circle(img, (x3, y3), 10, (0, 0, 255), cv2.FILLED)
-            cv2.circle(img, (x3, y3), 15, (0, 0, 255), 2)
-            cv2.putText(img, str(int(angle)), (x2 - 50, y2 + 50),
-                        cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+            cv.line(img, (x1, y1), (x2, y2), (255, 255, 255), 3)
+            cv.line(img, (x3, y3), (x2, y2), (255, 255, 255), 3)
+            cv.circle(img, (x1, y1), 10, (0, 0, 255), cv.FILLED)
+            cv.circle(img, (x1, y1), 15, (0, 0, 255), 2)
+            cv.circle(img, (x2, y2), 10, (0, 0, 255), cv.FILLED)
+            cv.circle(img, (x2, y2), 15, (0, 0, 255), 2)
+            cv.circle(img, (x3, y3), 10, (0, 0, 255), cv.FILLED)
+            cv.circle(img, (x3, y3), 15, (0, 0, 255), 2)
+            cv.putText(img, str(int(angle)), (x2 - 50, y2 + 50),
+                        cv.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
         return angle
     
     
 def main():
-    cap = cv2.VideoCapture('trainingVids/curls_1.mp4')
+    cap = cv.VideoCapture('trainingVids/curls_1.mp4')
     pTime = 0
     detector = poseDetector()
     while True:
         success, img = cap.read()
-        img = cv2.resize(img, (1280,720))
+        img = cv.resize(img, (1280,720))
         img = detector.findPose(img)
         lmList = detector.findPosition(img, draw=False)
         if len(lmList) != 0:
             print(lmList[14])
-            #cv2.circle(img, (lmList[14][1], lmList[14][2]), 15, (0, 0, 255), cv2.FILLED)
+            #cv.circle(img, (lmList[14][1], lmList[14][2]), 15, (0, 0, 255), cv.FILLED)
         cTime = time.time()
         fps = 1 / (cTime - pTime)
         pTime = cTime
-        cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3,
+        cv.putText(img, str(int(fps)), (70, 50), cv.FONT_HERSHEY_PLAIN, 3,
                     (255, 0, 0), 3)
-        cv2.imshow("Image", img)
-        cv2.waitKey(1)
+        cv.imshow("Image", img)
+        cv.waitKey(1)
 if __name__ == "__main__":
     main()
